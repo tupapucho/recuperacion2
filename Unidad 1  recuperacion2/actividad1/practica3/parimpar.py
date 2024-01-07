38160ffdee3d1ed07047de93b86a815eaf4d527f
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2024-01-06 23:34:11 -0600

    Update parimpar.py

---
+++ Unidad 1  recuperacion2/actividad1/practica3/parimpar.py
@@ -0,0 +1,12 @@
+'''
+Autor: Antonio Uribe Ramirez
+fehca:01/01/2024
+problema: par e impar
+'''
+A = int(input())
+if A == 0:
+    print("Nulo")
+elif A % 2 == 0:
+    print("Par")
+else:
+    print("Impar")
