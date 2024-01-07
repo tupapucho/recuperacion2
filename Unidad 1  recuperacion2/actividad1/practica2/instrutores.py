0885acacedcb83ef0cadfbe191346ae6ee1c5cf5
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2024-01-06 23:32:31 -0600

    Update instrutores.py

---
+++ Unidad 1  recuperacion2/actividad1/practica2/instrutores.py
@@ -0,0 +1,8 @@
+'''
+Autor: Antonio Uribe Ramirez
+fehca:01/01/2024
+problema: instrutores
+'''
+nombres_instructores = input().split()
+cantidad_instructores = len(nombres_instructores)
+print(cantidad_instructores)
