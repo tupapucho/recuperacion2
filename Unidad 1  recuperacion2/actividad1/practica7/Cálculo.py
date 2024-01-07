4b549df0a6d2dd678eb63d99d395bc7d1de7a9f0
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2024-01-06 23:35:44 -0600

    Update Cálculo.py

---
+++ Unidad 1  recuperacion2/actividad1/practica7/Cálculo.py
@@ -0,0 +1,27 @@
+import math
+
+def calcular_velocidad_promedio(fuerza_levantamiento, densidad_aire, coeficiente_levantamiento, superficie_alar):
+
+    velocidad = math.sqrt((2 * fuerza_levantamiento) / (densidad_aire * coeficiente_levantamiento * superficie_alar))
+    return velocidad
+
+def main():
+
+    sets = input().strip().split()
+
+    velocidades = []
+
+    for set_parametros in sets:
+
+        parametros = list(map(float, set_parametros.split(',')))
+
+        velocidad = calcular_velocidad_promedio(*parametros)
+
+        velocidades.append(velocidad)
+
+    velocidad_promedio = math.ceil(math.fsum(velocidades) / len(velocidades))
+
+    print(velocidad_promedio)
+
+if __name__ == "__main__":
+    main()
