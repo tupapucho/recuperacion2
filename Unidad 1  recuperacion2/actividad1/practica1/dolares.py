5170bae6890e8cc85da4e8df1f444fa83f622a8c
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2024-01-09 16:05:58 -0600

    Update dolares.py

--- Unidad 1  recuperacion2/actividad1/practica1/dolares.py
+++ Unidad 1  recuperacion2/actividad1/practica1/dolares.py
@@ -1,3 +1,8 @@
+'''
+Autor: Antonio Uribe Ramirez
+fehca:01/01/2024
+problema: dolares
+'''
 calificaciones = list(map(int, input().split()))
 promedio = sum(calificaciones) / len(calificaciones)
 print(round(promedio))
