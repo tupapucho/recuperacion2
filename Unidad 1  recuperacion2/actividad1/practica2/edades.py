2d6cf5877d097fc569b1f217e6e3d9fe5a274daa
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2024-01-09 16:11:16 -0600

    Update edades.py

--- Unidad 1  recuperacion2/actividad1/practica2/edades.py
+++ Unidad 1  recuperacion2/actividad1/practica2/edades.py
@@ -1,4 +1,13 @@
+'''
+Autor: Antonio Uribe Ramirez
+fehca:01/01/2024
+problema: edades 
+'''
 edades = list(map(int, input().split()))
 edades_set = set(edades)
 edades_ordenadas = sorted(edades_set, reverse=True)
 print(edades_ordenadas)
+
+'''
+no funciona
+'''
