305305c911cc0e49fa288315284fad2d7cca22b8
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2024-01-16 10:26:02 -0600

    Update tazon de frutas.py

--- actividad2/tazon de frutas.py
+++ actividad2/tazon de frutas.py
@@ -1,7 +1,8 @@
 '''
 Autor: Antonio Uribe Ramirez
 fehca:10/01/2024
-problema: suma de dos numeros
+problema: tazon de frutas
+
 '''
 cantidad_total_fruta = int(input())
 pays_completos_mango = cantidad_total_fruta // 6
