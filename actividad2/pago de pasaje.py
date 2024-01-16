35259289d67f8536df6ee104b81d6c132b247c9b
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2024-01-16 10:21:43 -0600

    Update pago de pasaje.py

--- actividad2/pago de pasaje.py
+++ actividad2/pago de pasaje.py
@@ -1,3 +1,8 @@
+'''
+Autor: Antonio Uribe Ramirez
+fehca:10/01/2024
+problema: pago de pasaje
+'''
 precio_primera_caseta = float(input())
 total_pagado = 0
 precios_casetas = [precio_primera_caseta]
